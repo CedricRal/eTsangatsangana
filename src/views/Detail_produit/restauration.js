f8f19318b9f40c4603c5671d2dd2bdc32b33ad3c
Cedric Ral <cedricvinamaster@gmail.com>
2022-11-16 18:57:50 +0300

    SPRINT4 Design Détails Produits

--- src/views/Detail_produit/restauration.js
+++ src/views/Detail_produit/restauration.js
@@ -12,23 +12,30 @@ import {
   TouchableOpacity
 } from 'react-native'; 
 import Button from '../Composant/bouton';
+import design from './../Composant/couleur';
 
 function Restaurant({navigation}) {
 
   return (  
     <>
     <ScrollView>
-    <View style={styles.container}>
-      <View style={styles.title_container}>
-        <Text style={styles.text_title}>
-          {restaurant.name}
-        </Text>
-      </View>
-      <View style={styles.img_container}>
-      <Image source={restaurant.img} style={styles.images}/>
-      </View>
+    <View>
+      
+      <ScrollView horizontal>
+        <View style={styles.img_container}>
+        <Image source={restaurant.img1} style={styles.images}/>
+        </View>
+        <View style={styles.img_container}>
+        <Image source={restaurant.img2} style={styles.images}/>
+        </View>
+        <View style={styles.img_container}>
+        <Image source={restaurant.img3} style={styles.images}/>
+        </View>
+      </ScrollView>
       <View style={styles.body_container}>
-        <Text style={styles.title_details}>{restaurant.name}</Text>
+      <Text style={styles.title_details}>{restaurant.produit}</Text>
+        <Text style={styles.text_title}>{restaurant.name}</Text>
+        <Text style={styles.prix}>{restaurant.prix}</Text>
         <Text style={styles.texte_center}>{restaurant.def}</Text>
         <Text style={styles.texte_center}>Contact: +{restaurant.tel}</Text>
         <Text style={styles.texte_center}>Siège: {restaurant.lieu}</Text>
@@ -46,6 +53,8 @@ function Restaurant({navigation}) {
 
 const restaurant = {
   name : 'KFC Madagascar',
+  produit: 'Nom du produit',
+  prix: '21 000 Ar',
   def : 'restaurantt spécialiste en poulet',
   tel : 261367364744,
   desc : 'Lorem commodo culpa ullamco incididunt minim fugiat velit pariatur officia. Do ut amet sit mollit commodo elit.',
@@ -54,6 +63,9 @@ const restaurant = {
   promo : 'Nuggets  -15% soit 21 000ar',
   cat_srv : 'hdtd',
   img : require('../../assets/images/KFC.jpg'),
+  img1: require('../../assets/images/Burger/IMG_5765.jpg'),
+  img2: require('../../assets/images/Burger/IMG_5781.jpg'),
+  img3: require('../../assets/images/Burger/IMG_5782.jpg'),
   boutton : 'Passer une commande' 
 } 
 export default Restaurant;
@@ -61,43 +73,35 @@ export default Restaurant;
 
 const styles = StyleSheet.create({
   images:{width:'100%', height:200},
-  title_details:{fontSize:30, fontWeight:'bold', textAlign:'center', marginVertical: 10},
-  texte_center:{textAlign: 'center', marginVertical: 4},
-  container : {
-    flex : 1,
-    marginHorizontal : 10
-  },
-  title_container : {
-    flex : 0.5,
-    backgroundColor : 'skyblue',
-  },
+  title_details:{color:design.Marron ,fontSize:28, fontWeight:'bold', textAlign:'center', marginVertical: '4%', fontFamily:design.police},
+  texte_center:{textAlign: 'center', marginVertical: '2%', fontFamily:design.police},
+  
   text_title : {
-    fontSize:30,
-    fontWeight:'bold',
-    color:'white',
+    fontSize:28,
+    color:'black',
     textAlign:'center',
-    marginTop : 5
+    textAlignVertical:'center',
+    fontFamily:design.police
   },
   img_container : {
-    flex : 3,
-  },
-  tex_body : {
-    fontSize: 15,
-    color:'black'
+    width:340,
+    height:'100%'
   },
   body_container : {
-    flex : 2.5,
     backgroundColor : 'white',
-    textAlign : 'center'
+    textAlign : 'center',
+    marginTop :-20,
+    borderRadius: 20
   },
   text_body : {
     fontSize: 15,
     color:'black',
-    margin:5
+    margin:5,
+    fontFamily:design.police
   },
   description : {
   
-    borderColor: 'black',
+    borderColor: design.Marron,
     borderWidth: 1,
     marginVertical: 12,
     marginHorizontal: 30,
@@ -105,20 +109,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     fontSize: 16
   },
-  appButtonContainer: {
-    elevation: 8,
-    backgroundColor: 'tomato',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    width: 320,
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: "#fff",
-    fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase"
+  prix : {
+    fontSize:28,
+    fontWeight:'bold',
+    color:design.Marron,
+    textAlign:'center',
+    textAlignVertical:'center',
+    fontFamily:design.police
   }
-  
   });
