d30825f463f524d56397f2691f2b6ee84607bdd0
NANANDO06 <andovinamaster@gmail.com>
2022-11-23 20:55:21 +0300

    Authentification_user

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,5 +1,6 @@
 const {GraphQLSchema, GraphQLObjectType,GraphQLID,GraphQLString} = require('graphql')
 const { add_users } = require('./mutation/inscription_user')
+const {auth_user} = require('./query/authentification_user')
 const {get_user} = require('./query/get_user')
 
 const queryAll = new GraphQLObjectType({
@@ -12,7 +13,8 @@ const queryAll = new GraphQLObjectType({
 const mutationAll = new GraphQLObjectType({
     name: 'mutation',
     fields: () => ({
-        add_users
+        add_users,
+        auth_user
     })
 })
 
