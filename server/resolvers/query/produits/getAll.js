f6e3f0c38fd12d0f9979f81748f65a6157fa6d88
NANANDO06 <andovinamaster@gmail.com>
2023-01-10 13:19:47 +0300

    Gestion de pagination

--- server/resolvers/query/produits/getAll.js
+++ server/resolvers/query/produits/getAll.js
@@ -1,15 +1,26 @@
 const client = require('../../../services/connection')
-
 module.exports = {
-    getAllProduit(parent,args){
+    getAllProduit(root,args){
         try{
-            return new Promise((resolve,reject)=>{
-                client.query('SELECT * FROM "Produits"',[],function(err,result){
+            return new Promise((resolve,reject) =>{
+                client.query('SELECT * FROM "Produits" LIMIT 10 OFFSET $1',[(args.page*10)],function(err,result){
                     if (err){
-                        reject(new Error(err))
+                        reject(err)
                     }
                     else{
-                        resolve(result.rows)
+                        const res = result.rows
+                        
+                        resolve({nbr_page: (new Promise((resolve,reject) =>{
+                            client.query('SELECT COUNT(*) FROM "Produits"',[],function(err,result){
+                                if (err){
+                                    reject(err)
+                                }
+                                else{                                    
+                                    resolve(parseInt(result.rows[0]['count']/10))
+                                }
+                            })
+                        })
+                        ) , items:res})
                     }
                 })
             })
--- server/resolvers/query/publicités/getAll.js
+++ server/resolvers/query/publicités/getAll.js
@@ -1,15 +1,26 @@
 const client = require('../../../services/connection')
-
 module.exports = {
     getAllPublicites(root,args){
         try{
             return new Promise((resolve,reject) =>{
-                client.query('SELECT * FROM "Publicités"',[],function(err,result){
+                client.query('SELECT * FROM "Publicités" LIMIT 10 OFFSET $1',[(args.page*10)],function(err,result){
                     if (err){
                         reject(err)
                     }
                     else{
-                        resolve(result.rows)
+                        const res = result.rows
+                        
+                        resolve({nbr_page: (new Promise((resolve,reject) =>{
+                            client.query('SELECT COUNT(*) FROM "Publicités"',[],function(err,result){
+                                if (err){
+                                    reject(err)
+                                }
+                                else{                                    
+                                    resolve(parseInt(result.rows[0]['count']/10))
+                                }
+                            })
+                        })
+                        ) , items:res})
                     }
                 })
             })
